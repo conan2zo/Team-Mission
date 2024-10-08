ce73259aed1b80fd65fd73ea0c397373f0df1b2c
Jung-Eunmi <dmssl2486@gmail.com>
2024-10-08 16:42:36 +0900

    View 추가

--- mybatis-Team-Mission/src/main/java/com/conan2zo/teamMission1008/view/JEMEmployeeView.java
+++ mybatis-Team-Mission/src/main/java/com/conan2zo/teamMission1008/view/JEMEmployeeView.java
@@ -1,10 +1,13 @@
 package com.conan2zo.teamMission1008.view;
 
+import java.util.Scanner;
+
 public class JEMEmployeeView {
 
     public void displqy() {
 
-
+        Scanner sc = new Scanner(System.in);
+        
 
 
     }
